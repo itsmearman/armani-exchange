360048da5c8a93a306894cabff091041c529d6e1
Arman Tehrani <arman.t.tehrani@gmail.com>
2024-11-04 17:53:50 +0330

    set font and icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div className="text-center py-[25%] ">
-        ok
+    <div className="grid gap-y-10 text-center py-[25%] ">
+      <p className="font-IranSans font-light">
+        سلام این یک متن تست با فونت ایران سنس است
+        <br/>
+        hello this is a test text with iransans font 
+      </p>
+      <p>
+      سلام این یک متن تست با فونت ایران سنس نیست
+      <br/>
+      hello this is a test text without iransans font
+      </p>
     </div>
   );
 }
