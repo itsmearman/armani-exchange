98cff2f6c2f420740b0249a8191791fd68e4c821
Arman Tehrani <arman.t.tehrani@gmail.com>
2024-11-08 21:09:53 +0330

    path config and  logo

--- src/view/layouts/navbar/navbar.tsx
+++ src/view/layouts/navbar/navbar.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavbarItem } from './navbarItem';
 import Link from 'next/link';
-
+import Image from 'next/image';
+import Logo from '@/public/Logo1.png'
 export default function Navbar() {
     const item = NavbarItem;
     return (
-        <div className='font-IranSans h-[5rem] fixed bg-red-400 w-full flex justify-center gap-[6rem]'>
+        <div className='font-IranSans h-[5rem] fixed bg-red-400 w-full flex justify-center gap-12'>
+            <Link href={"/"}>
+            <Image src={Logo} width={50} height={50} alt='' className='my-[0.8rem] w-auto' />
+            </Link>
             {item.map((data: any, index) => (
                 <Link href={data.route} key={index} className='my-[1.8rem]'>
                     {data.title}
