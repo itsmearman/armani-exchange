bea8beb48fc00acfd1770defa5ad931dc43df323
TarmanT <arman.t.tehrani@gmail.con>
2024-11-29 21:20:10 +0330

    next intl init

--- src/view/notFound/index.tsx
+++ src/view/notFound/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { useTranslations } from 'next-intl'
 export default function NotFoundView() {
+  const t = useTranslations();
   return (
     <div className='h-[46.5rem] flex flex-col items-center justify-center bg-gray-100 text-2xl'>
       <p className='font-bold text-4xl'>404</p>
-      <p className=''>NotFound</p>
+      <p className=''>{t("notFound")}</p>
     </div>
   )
 }
