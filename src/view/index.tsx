1012d718a19c0bccd11b5214efee9a5468610180
Arman Tehrani <arman.t.tehrani@gmail.com>
2024-12-06 12:35:09 +0330

    landing init

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { useTranslations } from "next-intl";
+import HomeTrade from "./home";
 export default function Home() {
-  const t = useTranslations();
   return (
-    <div className="h-[46.5rem]  pt-[8rem] text-center md:pt-[15rem]">
-      <div className="grid gap-y-10">
-      <p className="font-light">
-        {t("homeText")}
-      </p>
-      </div>
-    </div>
+    <HomeTrade />
   );
 }
