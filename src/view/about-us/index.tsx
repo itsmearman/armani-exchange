f77342aa27bb4ed3a75b7b28c363c37b8f5ebfef
Arman Tehrani <arman.t.tehrani@gmail.com>
2024-12-10 09:09:07 +0330

    h-svh

--- src/view/about-us/index.tsx
+++ src/view/about-us/index.tsx
@@ -4,7 +4,7 @@ import { useTranslations } from "next-intl";
 export default function AbutUsVeiw() {
   const t = useTranslations();
   return (
-    <div className="h-svh md:h-[38.2rem] flex flex-col gap-5 items-center justify-center bg-gray-50 text-2xl">
+    <div className="h-svh flex flex-col gap-5 items-center justify-center bg-gray-50 text-2xl">
       <div className="flex flex-col text-center gap-2">{t("phone")}<a href="tel:09212871700" className="text-green-500 hover:text-blue-500">09212871700</a></div>
       <div className="flex flex-col text-center gap-2">{t("email")}<a href="mailto:arman.t.tehrani@gmail.com" className="text-blue-500 hover:text-green-500">arman.t.tehrani@gmail.com</a></div>
     </div>
