647238c0a78949c4af8a1365ba24af2ef01d51da
Arman Tehrani <arman.t.tehrani@gmail.com>
2025-01-18 18:29:50 +0330

    package | slider

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,24 +1,55 @@
-import React from 'react'
-import { useTranslations } from 'next-intl'
-import Link from 'next/link';
-export default function HomeTrade() {
-    const t = useTranslations();
-    return (
-        <div className="h-svh pt-[8rem] px-8">
-            <div className="grid gap-y-10">
-                <p className="text-center font-bold text-2xl text-green-600">
-                    {t("homeText")}
-                </p>
-                <p className="text-center font-bold text-2xl text-blue-600">
-                    {t("homeWelcomeText")}
-                </p>
-                <Link href={'/spot'} className='mx-auto w-[12rem] content-center text-center h-10 rounded-full shadow-md bg-green-500 text-white hover:bg-blue-500 transition-all duration-100'>
-                    {t("homeSpotButtonText")}
-                </Link>
-                <p className="text-center text-blue-500 mt-[-2rem]">
-                    {t("homeHelperText")}
-                </p>
-            </div>
-        </div>
-    )
+// import React from 'react'
+// import { useTranslations } from 'next-intl'
+// import Link from 'next/link';
+// export default function HomeTrade() {
+//     const t = useTranslations();
+//     return (
+//         <div className="h-svh pt-[8rem] px-8">
+//             <div className="grid gap-y-10">
+//                 <p className="text-center font-bold text-2xl text-green-600">
+//                     {t("homeText")}
+//                 </p>
+//                 <p className="text-center font-bold text-2xl text-blue-600">
+//                     {t("homeWelcomeText")}
+//                 </p>
+//                 <Link href={'/spot'} className='mx-auto w-[12rem] content-center text-center h-10 rounded-full shadow-md bg-green-500 text-white hover:bg-blue-500 transition-all duration-100'>
+//                     {t("homeSpotButtonText")}
+//                 </Link>
+//                 <p className="text-center text-blue-500 mt-[-2rem]">
+//                     {t("homeHelperText")}
+//                 </p>
+//             </div>
+//         </div>
+//     )
+// }
+
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SliderItem from "./sliderItem";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import { EffectCards } from "swiper/modules";
+import Link from "next/link";
+
+export default function App() {
+    const slider = SliderItem();
+  return (
+    <div className="py-[8rem]">
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="w-60 h-80"
+      >
+        {slider.map((data,index)=>(
+        <SwiperSlide key={index} className={`content-center text-center align-middle items-center rounded-[18px] text-2xl font-bold text-white bg-[rgb(${data.color})]`}>
+          <Link href={data.route}>
+          {data.title}
+          </Link>
+        </SwiperSlide>
+        ))
+        }
+      </Swiper>
+    </div>
+  );
 }
---
+++ src/view/home/sliderItem.ts
@@ -0,0 +1,57 @@
+import { useTranslations } from 'next-intl'
+export default function SliderItem(){
+const Slider = [
+    {
+        title:"title1",
+        color:"206,17,17",
+        route:"/spot",
+    },
+    {
+        title:"title2",
+        color:"0,140,255",
+        route:"/about-us",
+    },
+    // {
+    //     title:"title3",
+    //     color:"10,184,111",
+    //     route:"/spot",
+    // },
+    // {
+    //     title:"title4",
+    //     color:"211,122,7",
+    //     route:"/spot",
+    // },
+    // {
+    //     title:"title5",
+    //     color:"118,163,12",
+    //     route:"/spot",
+    // },
+    // {
+    //     title:"title6",
+    //     color:"180,10,47",
+    //     route:"/spot",
+    // },
+    // {
+    //     title:"title7",
+    //     color:"35,99,19",
+    //     route:"/spot",
+    // },
+    // {
+    //     title:"title8",
+    //     color:"0,68,255",
+    //     route:"/spot",
+    // },
+    // {
+    //     title:"title9",
+    //     color:"218,12,218",
+    //     route:"/spot",
+    // },
+    // {
+    //     title:"title10",
+    //     color:"54,94,77",
+    //     route:"/spot",
+    // },
+
+]
+return Slider;
+}
