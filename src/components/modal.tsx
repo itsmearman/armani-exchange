a07bc0ac477e6d7f2b2aa388310847cc56945bfc
Arman Tehrani <arman.t.tehrani@gmail.com>
2025-04-02 10:20:43 +0330

    Feature : after trade show done modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -17,7 +17,7 @@ export default function CustomModal({ isOpen, onClose, message }: ModalProps) {
         <p className="text-center">
           {message}
         </p>
-        <div className="flex justify-center mt-14" >
+        <div className="flex justify-center mt-4" >
           <button
             onClick={onClose}
             className="px-4 py-2 bg-blue-500 text-white rounded-lg border-none cursor-pointer transition-colors duration-300 ease-in-out hover:bg-blue-700">
