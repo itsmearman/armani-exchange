113ea77bd8c8dccc981de7f84ef5630737c159a6
Arman Tehrani <arman.t.tehrani@gmail.com>
2025-04-06 20:43:17 +0330

    Design : change design

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,10 +1,18 @@
-// pages/signup.js
 import SignUpForm from '@/src/components/SignUpForm'
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata() {
+  const t = await getTranslations();
+  
+  return {
+    title: t("SigninTitle"),
+  };
+}
 
 export default function SignUpPage() {
   return (
-    <div className="max-w-md mx-auto mt-20">
-      <h1 className="text-xl font-bold mb-4">ثبت‌نام</h1>
+    <div className="max-w-md mx-auto mt-28">
+      <h1 className="text-xl font-bold mb-4 text-center">ثبت‌نام</h1>
       <SignUpForm />
     </div>
   )
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,16 +1,16 @@
-// components/SignUpForm.js
 'use client'
-import { useState } from 'react'
+import { useState, FormEvent } from 'react'
 import { supabase } from '@/lib/supabaseClient'
 
 export default function SignUpForm() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [message, setMessage] = useState(null)
+  const [message, setMessage] = useState<string | null>(null)
 
-  const handleSignUp = async (e) => {
+  const handleSignUp = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const { data, error } = await supabase.auth.signUp({
+
+    const { error } = await supabase.auth.signUp({
       email,
       password,
     })
@@ -18,7 +18,7 @@ export default function SignUpForm() {
     if (error) {
       setMessage(error.message)
     } else {
-      setMessage('لینک تایید به ایمیل شما ارسال شد.')
+      setMessage('لینک تأیید به ایمیل شما ارسال شد.')
     }
   }
 
@@ -38,10 +38,12 @@ export default function SignUpForm() {
         onChange={(e) => setPassword(e.target.value)}
         className="border p-2 w-full"
       />
-      <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+      <div className='w-full flex justify-center pb-5'>
+      <button type="submit" className="w-11/12 bg-blue-500 text-white p-2 rounded">
         ثبت‌نام
       </button>
       {message && <p>{message}</p>}
+      </div>
     </form>
   )
 }
