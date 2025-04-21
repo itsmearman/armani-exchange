e1a1b7c212db6d6e0e9250d6a75046d1b4b3ebab
Arman Tehrani <arman.t.tehrani@gmail.com>
2025-04-21 12:42:40 +0330

    Debug : build error

--- src/view/home/home.tsx
+++ src/view/home/home.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 import * as THREE from "three";
