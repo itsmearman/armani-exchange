9c06d5130f0225709f6203e93396fef3b2edd29a
armani <arman.t.tehrani@gmail.com>
2025-04-24 18:57:46 +0330

    ADD : change about us

--- src/view/about-us/aboutUs.tsx
+++ src/view/about-us/aboutUs.tsx
@@ -4,8 +4,9 @@ import { useTranslations } from "next-intl";
 export default function AbutUsVeiw() {
   const t = useTranslations();
   return (
-    <div className="h-svh flex flex-col gap-5 items-center justify-center text-2xl">
-      <div className="flex flex-col text-center gap-2">{t("email")}<a href="mailto:arman.t.tehrani@gmail.com" className="text-blue-500 hover:text-green-500">arman.t.tehrani@gmail.com</a></div>
+    <div className="h-svh flex flex-col gap-5 items-center justify-center text-2xl text-justify">
+      <p className="w-1/2 ">{t("abouttext")}</p>
+      <div className="flex flex-row text-center gap-2">{t("email")} :<a href="mailto:arman.t.tehrani@gmail.com" className="text-blue-500 hover:text-green-500">arman.t.tehrani@gmail.com</a></div>
     </div>
   );
 }
